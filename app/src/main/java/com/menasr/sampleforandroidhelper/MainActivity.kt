4b90949801626d816e38aaef1b7ea1773ddf6cef
ranjandas503 <ranjan@techphant.com>
2019-05-21 17:09:25 +0530

    few added

--- app/src/main/java/com/menasr/sampleforandroidhelper/MainActivity.kt
+++ app/src/main/java/com/menasr/sampleforandroidhelper/MainActivity.kt
@@ -2,7 +2,9 @@ package com.menasr.sampleforandroidhelper
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
 import com.menasr.androidhelper.Andy
+import com.menasr.androidhelper.ResourceLoader
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +17,12 @@ class MainActivity : AppCompatActivity() {
 
         sampleText.text= Andy.res.string(R.string.app_name)
         sampleText.setTextColor(Andy.res.color(R.color.colorAccent))
+        sampleText.textSize = Andy.res.dimen(R.dimen.text).toString().toFloat()
+
 
         image.setImageDrawable(Andy.res.drawable(R.drawable.ic_directions_bike_black_24dp))
+//        image.animation = Andy.res.anim(R.anim.rotate)
+//        image.animate().start()
+//        Andy.res.startAnim(R.anim.rotate,image)
     }
 }
